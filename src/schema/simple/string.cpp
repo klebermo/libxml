678947a78760152f165eda1b92082096302bf272
Kleber Mota de Oliveira <klebermo@gmail.com>
2024-08-11 20:31:10 -0300

    Update 11/08/2024 20:31

--- src/schema/simple/string.cpp
+++ src/schema/simple/string.cpp
@@ -2,132 +2,33 @@
 
 std::string String::print() {
     std::string result;
-    switch(preffix) {
-        case Zero: result = result + ""; break;
-        case Virgula: result = result + ","; break;
-        case Ponto_e_virgula: result = result + ";"; break;
-        case Dois_pontos: result = result + ":"; break;
-        case Reticencias: result = result + "..."; break;
-        case Parenteses_abertura: result = result + "("; break;
-        case Parenteses_fechamento: result = result + ")"; break;
-        case Aspas: result = result + "\""; break;
-        case Travessao: result = result + "-"; break;
-        case Ponto_de_exclamacao: result = result + "!"; break;
-        case Ponto_de_interrogacao: result = result + "?"; break;
-        case Ponto_final: result = result + "."; break;
-    }
-    result = result + value.tokens.data();
-    switch(suffix) {
-        case Zero: result = result + ""; break;
-        case Virgula: result = result + ","; break;
-        case Ponto_e_virgula: result = result + ";"; break;
-        case Dois_pontos: result = result + ":"; break;
-        case Reticencias: result = result + "..."; break;
-        case Parenteses_abertura: result = result + "("; break;
-        case Parenteses_fechamento: result = result + ")"; break;
-        case Aspas: result = result + "\""; break;
-        case Travessao: result = result + "-"; break;
-        case Ponto_de_exclamacao: result = result + "!"; break;
-        case Ponto_de_interrogacao: result = result + "?"; break;
-        case Ponto_final: result = result + "."; break;
-    }    
+    result = result + preffix;
+    for(char ch : value.tokens)
+        result = result + ch;
+    result = result + suffix;
     return result;
 }
 
 void String::read(std::string data) {
     char ch;
     std::string punctuation = ",;:()\"—!?.";
-    value.tokens.clear();
-
-    // Ler e identificar o prefixo
     std::stringstream is(data);
+
+    preffix = 0x0;
+    suffix = 0x0;
+
     if (punctuation.find(is.peek()) != std::string::npos) {
         is >> ch;
-        switch (ch) {
-            case ',':
-                preffix = Virgula;
-                break;
-            case ';':
-                preffix = Ponto_e_virgula;
-                break;
-            case ':':
-                preffix = Dois_pontos;
-                break;
-            case '(':
-                preffix = Parenteses_abertura;
-                break;
-            case ')':
-                preffix = Parenteses_fechamento;
-                break;
-            case '"':
-                preffix = Aspas;
-                break;
-            case '-':
-                preffix = Travessao;
-                break;
-            case '!':
-                preffix = Ponto_de_exclamacao;
-                break;
-            case '?':
-                preffix = Ponto_de_interrogacao;
-                break;
-            case '.':
-                if(is.peek() == '.')
-                    preffix = Reticencias;
-                else
-                    preffix = Ponto_final;
-                break;
-            default:
-                preffix = Zero;
-                break;
-        }
+        preffix = ch;
     }
 
-    // Ler a palavra
-    while (is >> ch && ch != ' ' && punctuation.find(ch) == std::string::npos) {
+    while (punctuation.find(is.peek()) == std::string::npos) {
+        is >> ch;
         value.tokens.push_back(ch);
     }
 
-    // Identificar o sufixo ou espaço no final
     if (punctuation.find(is.peek()) != std::string::npos) {
         is >> ch;
-        switch (ch) {
-            case ',':
-                suffix = Virgula;
-                break;
-            case ';':
-                suffix = Ponto_e_virgula;
-                break;
-            case ':':
-                suffix = Dois_pontos;
-                break;
-            case '(':
-                suffix = Parenteses_abertura;
-                break;
-            case ')':
-                suffix = Parenteses_fechamento;
-                break;
-            case '"':
-                suffix = Aspas;
-                break;
-            case '-':
-                suffix = Travessao;
-                break;
-            case '!':
-                suffix = Ponto_de_exclamacao;
-                break;
-            case '?':
-                suffix = Ponto_de_interrogacao;
-                break;
-            case '.':
-                if(is.peek() == '.')
-                    suffix = Reticencias;
-                else
-                    suffix = Ponto_final;
-                break;
-            default:
-                suffix = Zero;
-                break;
-        }
+        suffix = ch;
     }
 }
--- src/schema/simple/string.hpp
+++ src/schema/simple/string.hpp
@@ -3,30 +3,16 @@
 
 #include "../simple.hpp"
 
-enum Pontuacao {
-    Zero,
-    Virgula,
-    Ponto_e_virgula,
-    Dois_pontos,
-    Reticencias,
-    Parenteses_abertura,
-    Parenteses_fechamento,
-    Aspas,
-    Travessao,
-    Ponto_de_exclamacao,
-    Ponto_de_interrogacao,
-    Ponto_final
-};
-
 struct Word {
     std::vector<char> tokens;
 };
+typedef struct Word Word;
 
 class String : public simpleType {
 private:
-    enum Pontuacao preffix;
-    struct Word value;
-    enum Pontuacao suffix;
+    char preffix;
+    Word value;
+    char suffix;
 public:
     std::string print();
     void read(std::string data);
